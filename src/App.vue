b25d0b6f3e6f5b6d864d1684592311fc81060b3f
liuqiangh <13020061876@163.com>
2019-07-27 19:20:00 +0800

    version2.0

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+html,
+body {
+  background-image: url("./static/img/bg01.png");
+}
+</style>
