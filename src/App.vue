4e161dd5efbf466b7579bb0797c494e9a1e27bf8
liuqiangh <13020061876@163.com>
2019-07-29 22:06:01 +0800

    改漂亮点儿
    研究列表还没想好

--- src/App.vue
+++ src/App.vue
@@ -14,5 +14,11 @@ export default {
 html,
 body {
   background-image: url("./static/img/bg01.png");
+  /* width: 100%;
+  height: 100%;
+  background-image: url("./static/img/bg01.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100% 100%; */
 }
 </style>
